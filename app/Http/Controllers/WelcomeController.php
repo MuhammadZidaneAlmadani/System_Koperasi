d9e1f67355fae38f7db0b2c80c0e7c5cad4d6efe
Doxxy02 <160561397+Doxxy02@users.noreply.github.com>
2024-09-20 17:21:04 +0700

    membuat dashboard

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -8,12 +8,23 @@ class WelcomeController extends Controller
 {
     public function index(){
         $breadcrumb = (object)[
-            'title'=> 'Selamat Datang',
-            'list'=>['Home','Welcome']
+            'title'=> 'Sistem Koperasi',
+            'list'=>['Home', 'Dashboard']
         ];
-        $activeMenu='dashboard';
-        return view ('welcome',['breadcrumb'=>$breadcrumb, 'activeMenu'=>$activeMenu]);
+        $activeMenu = 'dashboard';
+        
+        // Simulasi data dari gambar
+        $data = [
+            'manajemen pengguna' => 2,
+            'data member' => 2,
+            'tabunganSaldo' => 375000,
+            'tabunganDebit' => 450000,
+            'tabunganKredit' => 75000,
+            'transaksiKredit' => 2,
+            'uangKreditAdmin' => 28000,
+            'uangKreditBelumBayar' => 151800,
+        ];
+        
+        return view('dashboard', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu, 'data' => $data]);
     }
 }
-
-
